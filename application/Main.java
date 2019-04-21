675909b014ce9f44d549213cd9d8ea2b2122634f
张元博 <zhangyuanbo@zhangyuanbodeMacBook-Pro.local>
2019-04-21 18:17:19 -0500

    Try sth

--- application/Main.java
+++ application/Main.java
@@ -6,12 +6,14 @@ import javafx.stage.Stage;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
 
 public class Main extends Application {
@@ -27,12 +29,32 @@ public class Main extends Application {
     private Node createHelpButton()
     {
       Image helpIcon = new Image(getClass().getResourceAsStream("/img/help.png"));
+      ImageView helpImageView = new ImageView(helpIcon);
+      helpImageView.setFitHeight(50);
+      helpImageView.setFitWidth(50);
       Button helpButton = new Button();
-      helpButton.setGraphic(new ImageView(helpIcon));
-      
+      helpButton.setGraphic(helpImageView);
       return helpButton;
     }
     
+    private Node createTextForComboBox()
+    {
+    	Text text = new Text();
+    	text.setText("size:");
+    	
+		return text;
+    }
+    
+    private Node createComboBox()
+    {
+    	ComboBox <String> comboBox = new ComboBox<String>();
+		comboBox.getItems().addAll(
+			    "5*5",
+			    "6*6",
+			    "7*7"
+			);
+		return comboBox;
+    }
     
 	@Override
 	public void start(Stage primaryStage) {
