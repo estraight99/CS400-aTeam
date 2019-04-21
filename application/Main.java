67169c8602c927eb33c671647fe9b1c730932d6a
dvbui <dvbui@wisc.edu>
2019-04-21 18:49:05 -0500

    Minor fix in createImageView()

--- application/Main.java
+++ application/Main.java
@@ -13,11 +13,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-<<<<<<< HEAD
 import javafx.scene.layout.VBox;
-=======
 import javafx.scene.layout.GridPane;
->>>>>>> 0bfe1d1f80b303c87e0240ee3c3895505c979126
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
