b812b382680ba855a64db04a9f612f59d4bb78aa
dvbui <dvbui@wisc.edu>
2019-04-21 17:34:43 -0500

    Add createTop() function

--- application/Main.java
+++ application/Main.java
@@ -2,17 +2,28 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+  
+    private Node createTitle()
+    {
+      Label title = new Label("Road Builder")
+      
+    }
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			root.setTop(createTop());
+			
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
