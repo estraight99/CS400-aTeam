a59d09e98241b783370792cb77d0a409d6172b32
dvbui <dvbui@wisc.edu>
2019-04-25 14:51:33 -0500

    remove unimportant packages

--- application/RightPanel.java
+++ application/RightPanel.java
@@ -1,14 +1,9 @@
 package application;
 
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
 /**
