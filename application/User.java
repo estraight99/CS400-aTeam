f40b8aaa9dea8a7b9079367407061eb17b219c51
dvbui <dvbui@wisc.edu>
2019-04-26 18:29:25 -0500

    Finish class User

--- application/User.java
+++ application/User.java
@@ -1,5 +1,30 @@
 package application;
 
 public class User {
-
+  private String name;
+  private int money;
+  public User(String name, int money)
+  {
+    this.name = name;
+    this.money = money;
+  }
+  
+  public void pay(int x)
+  {
+    this.money -= x;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public int getMoney() {
+    return money;
+  }
+  public void setMoney(int money) {
+    this.money = money;
+  }
+  
 }
