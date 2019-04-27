8b24ca9168b41739f6177a50de9c449c3411fa05
dvbui <dvbui@wisc.edu>
2019-04-26 20:58:35 -0500

    Prepare for map generation

--- application/MapGenerator.java
+++ application/MapGenerator.java
@@ -3,25 +3,24 @@ package application;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.Random;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class MapGenerator {
   static final String smallPath = "."+File.separator+"database"+File.separator+"smallMap.json";
   static final String mediumPath = "."+File.separator+"database"+File.separator+"mediumMap.json";
+  static final String bigPath = "."+File.separator+"database"+File.separator+"bigMap.json";
   public static void generateMap(String pathToJSONFile,int length,int width) throws FileNotFoundException
   {
     JSONObject jo = new JSONObject();
     jo.put("length", length);
     jo.put("width", width);
-    Random rand = new Random(10);
     
     JSONArray data = new JSONArray();
     for (int i=1; i<=length; i++)
       for (int j=1; j<=width; j++)
       {
-        JSONObject location = (new Location(i,j,"","",rand.nextInt(16))).createJSONObject();
+        JSONObject location = (new Location(i,j,"","",0)).createJSONObject();
         data.add(location);
       }
     
@@ -38,5 +37,7 @@ public class MapGenerator {
   {
     generateMap(smallPath,10,10);
     generateMap(mediumPath,1000,1000);
+    generateMap(bigPath,1000,10000);
+    System.out.println("Done!");
   }
 }
