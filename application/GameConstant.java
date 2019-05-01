ffb997696fc4d1988f9dc1a14b0dbe31567e8f01
张元博 <zhangyuanbo@zhangyuanbodeMacBook-Pro.local>
2019-04-30 19:37:23 -0500

    Commenting GameConstant

--- application/GameConstant.java
+++ application/GameConstant.java
@@ -8,10 +8,11 @@ import java.io.File;
  *
  */
 public class GameConstant {
-  public static int ROAD_COST = 1;
-  static int[] mx = new int[] {-1, 0, 1, 0};
-  static int[] my = new int[] {0, -1, 0, 1};
-  static final String smallPath = "."+File.separator+"database"+File.separator+"smallMap.json";
+  public static int ROAD_COST = 1; // the cost for each new road
+  static int[] mx = new int[] {-1, 0, 1, 0}; // x axis change for moveMap operation: up, left, down, right
+  static int[] my = new int[] {0, -1, 0, 1}; // y axis change for moveMap operation: up, left, down, right
+  // file path for each size of map
+  static final String smallPath = "."+File.separator+"database"+File.separator+"smallMap.json"; 
   static final String mediumPath = "."+File.separator+"database"+File.separator+"mediumMap.json";
   static final String bigPath = "."+File.separator+"database"+File.separator+"bigMap.json";
 }
