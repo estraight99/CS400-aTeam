a830576787f2016989b79ffbe180e9e6870c4c23
张元博 <zhangyuanbo@zhangyuanbodeMacBook-Pro.local>
2019-04-30 20:23:34 -0500

    commenting LegendView

--- application/LegendView.java
+++ application/LegendView.java
@@ -11,12 +11,19 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 
 /**
+ * This class defines the area where all the legends are displayed, it extends FlowPane class
  * 
  * @author Yuanbo Zhang
  *
  */
 public class LegendView extends FlowPane {
 	
+	/**
+	 * This method initialize a legendView where all the legends are displayed
+     * @throws FileNotFoundException is never thrown if no file is missing
+     * @throws IOException is never thrown if no file is missing
+     * @throws ParseException is never thrown if no file is missing
+     */
 	LegendView() throws FileNotFoundException, IOException, ParseException
 	{
 	    TypeOfLocationList list = new TypeOfLocationList();
@@ -33,6 +40,11 @@ public class LegendView extends FlowPane {
 		this.setMaxWidth(350.0);
 	}
 	
+	/**
+	 * This method loads a legend icon and return the fitted imageView
+	 * @param typeOfLocation the type of this map block
+     * @return an imageView of the legend icon
+     */
 	private ImageView createLegend(int typeOfLocation)
     {
       Image legend = new Image(getClass().getResourceAsStream(".."+File.separator+"img"+File.separator+"legend"+File.separator+typeOfLocation+".png"));
